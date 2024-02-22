501093231b765f7c19db08e85260ed255ecacb04
Mfxof <matysfreyermuth@gmail.com>
2024-02-22 11:22:55 +0100

    BackRem

---
+++ Temp.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Accueil</title>
+    <?php include 'php/header.php' ?>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <!-- Logo et options start -->
+        <div class="top-bar">
+            <div class="logo">
+                <img src="XXX.png" alt="Logo">
+            </div>
+            <div class="options">
+                <div class="language">
+                    <a href="#" class="flag-icon">🇺🇸</a>
+                    <a href="#" class="flag-icon">🇫🇷</a>
+                    <!-- Ajoutez d'autres drapeaux pour d'autres langues -->
+                </div>
+                <div class="currency">
+                    <a href="#">$</a>
+                    <a href="#">€</a>
+                    <!-- Ajoutez d'autres symboles de devise pour d'autres devises -->
+                </div>
+                <div class="wishlist">
+                    <a href="#">Liste de souhaits</a>
+                </div>
+                <div class="account">
+                    <a href="#">Mon compte</a>
+                </div>
+            </div>
+        </div>
+        <!-- Logo et options end -->
+
+        <!-- Home start -->
+        <section>
+          
+        </section>
+        <!-- Home end -->
+
+        <?php include 'php/navBar.php' ?>
+
+        <div class="forum">
+            <h1>Bienvenue sur le forum !</h1>
+            
+            <!-- Fil de discussion 1 -->
+            <div class="thread">
+                <h2>Titre du fil de discussion 1</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel aliquet libero. Fusce id ligula in nunc posuere tincidunt non ut dolor. Donec posuere arcu eget enim efficitur, id interdum justo vulputate.</p>
+                <p>Posté par : Utilisateur123 | Date : 22 février 2024</p>
+            </div>
+
+            <!-- Fil de discussion 2 -->
+            <div class="thread">
+                <h2>Titre du fil de discussion 2</h2>
+                <p>Sed quis consequat nisi. In id purus diam. Nunc nec luctus libero. Sed malesuada malesuada ex, nec malesuada ligula tincidunt vel.</p>
+                <p>Posté par : Utilisateur456 | Date : 21 février 2024</p>
+            </div>
+        </div>
+
+        <?php include 'php/footer.php' ?>
+    </div>
+</body>
+
+</html>
